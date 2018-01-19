7f5e69990d681dfa8a36af98715345817007fb5e
Troy Williams <troyDonwilliams@gmail.com>
2018-01-18 22:25:54 -0600

    completed

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,107 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [
+        {
+          name: 'Loading Friends...'
+        }
+      ],
+      newFriend: {
+        name: null,
+        age: null,
+        email: null
+      }
+    }
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAgeChange = this.handleAgeChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.addNewFriend = this.addNewFriend.bind(this);
+    this.deleteFriend = this.deleteFriend.bind(this);
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:5000/friends";
+    axios.get(url).then(response => this.setState({friends: response.data})).catch(err => console.log(err));
+  }
+
+  handleNameChange(e) {
+    const name = e.target.value;
+    this.setState({
+      newFriend: {
+        name,
+        age: this.state.newFriend.age,
+        email: this.state.newFriend.email
+      }
+    })
+  }
+
+  handleAgeChange(e) {
+    const age = e.target.value;
+    this.setState({
+      newFriend: {
+        age,
+        name: this.state.newFriend.name,
+        email: this.state.newFriend.email
+      }
+    })
+  }
+
+  handleEmailChange(e) {
+    const email = e.target.value;
+    this.setState({
+      newFriend: {
+        email,
+        name: this.state.newFriend.name,
+        age: this.state.newFriend.age
+      }
+    })
+  }
+
+  addNewFriend(e) {
+    e.preventDefault();
+    const newFriend = this.state.newFriend;
+    const url = "http://localhost:5000/friends";
+    //axios post url (info) then catch
+    axios.post(url, newFriend).then(response => {
+      this.setState({friends: response.data})
+    }).catch(err => console.log(err))
+  }
+
+  deleteFriend(e) {
+    const id = e.target.id;
+    const url = "http://localhost:5000/friends/";
+    axios.delete(url + id).then(response => {
+      this.setState({friends: response.data})
+    }).catch(err => console.log(err))
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div className="App">
+      <h1>App</h1>
+      <form onSubmit={this.addNewFriend}>
+        <input type='string' placeholder='Name' required="required" onChange={this.handleNameChange}></input>
+        <input type='number' placeholder='Age' required="required" onChange={this.handleAgeChange}></input>
+        <input type='email' placeholder='email' required="required" onChange={this.handleEmailChange}></input>
+        <button onClick={this.addNewFriend}>Add Friend</button>
+      </form>
+      <ul>
+        {
+          this.state.friends.map(friend => {
+            return <li key={friend.id}>
+              <div>{friend.name}</div>
+              <div>{friend.age}</div>
+              <div>{friend.email}</div>
+              <button onClick={this.deleteFriend} id={friend.id}>delete</button>
+            </li>
+          })
+        }
+      </ul>
+    </div>);
   }
 }
 
